316c928e7770b1e3f2e46de3707bfbe350ee989e
Jesus Cadena <j.cadena.real@gmail.com>
2018-06-18 12:26:09 +0100

    mobile menu added

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,14 @@
 "use strict";
+
+var backdrop = document.querySelector(".backdrop");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
+toggleButton.addEventListener("click", function () {
+  backdrop.classList.add("open");
+  mobileNav.classList.add("open");
+});
+backdrop.addEventListener("click", function () {
+  backdrop.classList.remove("open");
+  mobileNav.classList.remove("open");
+});
